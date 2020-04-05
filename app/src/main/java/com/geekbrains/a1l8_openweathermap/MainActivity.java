ddaea88178479221f1d06cc9d738b5a883f429d1
koassumo <0770407@gmail.com>
2020-04-05 17:04:35 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrains/a1l8_openweathermap/MainActivity.java
@@ -0,0 +1,206 @@
+package com.geekbrains.a1l8_openweathermap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private final Handler handler = new Handler();
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+
+    Typeface weatherFont;
+    private TextView cityTextView;
+    private TextView updatedTextView;
+    private TextView detailsTextView;
+    private TextView currentTemperatureTextView;
+    private TextView weatherIconTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        initFonts();
+    }
+
+    private void initViews() {
+        cityTextView = findViewById(R.id.city_field);
+        updatedTextView = findViewById(R.id.updated_field);
+        detailsTextView = findViewById(R.id.details_field);
+        currentTemperatureTextView = findViewById(R.id.current_temperature_field);
+        weatherIconTextView = findViewById(R.id.weather_icon);
+    }
+
+    private void initFonts() {
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weather.ttf");
+        weatherIconTextView.setTypeface(weatherFont);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        showInputDialog();
+        return true;
+    }
+
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.change_city);
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                updateWeatherData(input.getText().toString());
+                updateWeatherData("Moscow");
+            }
+        });
+        builder.show();
+    }
+
+
+
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            @Override
+            public void run() {
+                final JSONObject jsonObject = WeatherDataLoader.getJSONData(city);
+                if(jsonObject == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), R.string.place_not_found,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(jsonObject);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject jsonObject) {
+        Log.d(LOG_TAG, "json: " + jsonObject.toString());
+        try {
+            JSONObject details = jsonObject.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = jsonObject.getJSONObject("main");
+
+            setPlaceName(jsonObject);
+            setDetails(details, main);
+            setCurrentTemp(main);
+            setUpdatedText(jsonObject);
+            setWeatherIcon(details.getInt("id"),
+                    jsonObject.getJSONObject("sys").getLong("sunrise") * 1000,
+                    jsonObject.getJSONObject("sys").getLong("sunset") * 1000);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            Log.e(LOG_TAG, "One or more fields not found in the JSON data");
+        }
+    }
+
+    private void setPlaceName(JSONObject jsonObject) throws JSONException {
+        String cityText = jsonObject.getString("name").toUpperCase() + ", "
+                + jsonObject.getJSONObject("sys").getString("country");
+        cityTextView.setText(cityText);
+    }
+
+    private void setDetails(JSONObject details, JSONObject main) throws JSONException {
+        String detailsText = details.getString("description").toUpperCase() + "\n"
+                + "Humidity: " + main.getString("humidity") + "%" + "\n"
+                + "Pressure: " + main.getString("pressure") + "hPa";
+        detailsTextView.setText(detailsText);
+    }
+
+    private void setCurrentTemp(JSONObject main) throws JSONException {
+        String currentTextText = String.format(Locale.getDefault(), "%.2f",
+                main.getDouble("temp")) + "\u2103";
+        currentTemperatureTextView.setText(currentTextText);
+    }
+
+    private void setUpdatedText(JSONObject jsonObject) throws JSONException {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        String updateOn = dateFormat.format(new Date(jsonObject.getLong("dt") * 1000));
+        String updatedText = "Last update: " + updateOn;
+        updatedTextView.setText(updatedText);
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId / 100;
+        String icon = "";
+
+        if(actualId == 800) {
+            long currentTime = new Date().getTime();
+            if(currentTime >= sunrise && currentTime < sunset) {
+                icon = "\u2600";
+                //icon = getString(R.string.weather_sunny);
+            } else {
+                icon = getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch (id) {
+                case 2: {
+                    icon = getString(R.string.weather_thunder);
+                    break;
+                }
+                case 3: {
+                    icon = getString(R.string.weather_drizzle);
+                    break;
+                }
+                case 5: {
+                    icon = getString(R.string.weather_rainy);
+                    break;
+                }
+                case 6: {
+                    icon = getString(R.string.weather_snowy);
+                    break;
+                }
+                case 7: {
+                    icon = getString(R.string.weather_foggy);
+                    break;
+                }
+                case 8: {
+                    icon = "\u2601";
+                    // icon = getString(R.string.weather_cloudy);
+                    break;
+                }
+            }
+        }
+        weatherIconTextView.setText(icon);
+    }
+}
---
+++ app/src/main/java/com/geekbrains/a1l8_openweathermap/WeatherDataLoader.java
@@ -0,0 +1,43 @@
+package com.geekbrains.a1l8_openweathermap;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class WeatherDataLoader {
+    private static final String OPEN_WEATHER_API_KEY = "762ee61f52313fbd10a4eb54ae4d4de2";
+    private static final String OPEN_WEATHER_API_URL =
+            "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    private static final String KEY = "x-api-key";
+
+    static JSONObject getJSONData(String city) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_API_URL, city));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty(KEY, OPEN_WEATHER_API_KEY);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder rawData = new StringBuilder(1024);
+            String tempVariable;
+
+            while ((tempVariable = reader.readLine()) != null) {
+                rawData.append(tempVariable).append("\n");
+            }
+
+            reader.close();
+
+            JSONObject jsonObject = new JSONObject(rawData.toString());
+            if(jsonObject.getInt("cod") != 200) {
+                return null;
+            } else {
+                return jsonObject;
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
+}
